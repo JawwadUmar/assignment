8a71651c0f4d399b98f58e097e40f01970b63966
Jawwad Umar <you@example.com>
2024-01-16 21:24:41 +0530

    somewhat working

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+// In-memory array to store messages
+const messages = [];
+
+app.set('view engine', 'ejs'); // Set EJS as the templating engine
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log("Our server is running on 3000");
+});
+
+app.get('/', (req, res) => {
+  res.render('index', { messages: messages }); // Pass messages to the EJS template
+});
+
+// Endpoint to handle message submissions
+app.post('/', (req, res) => {
+  const messageData = req.body.message;
+  if (messageData) {
+    messages.push(messageData);
+    console.log(messageData);
+    res.status(200).send('Message submitted successfully!');
+  } else {
+    res.status(400).send('Bad Request: Message content is required.');
+  }
+});
